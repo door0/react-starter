72ac915cf2c2e2ee91a8d650ea308e9d2fd0e9e4
door0 <moonzerochoe@gmail.com>
2021-12-01 23:41:21 +0900

    Functonal Update Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Setup from './tutorial/1-useState/setup/5-useState-counter';
-import Final from './tutorial/1-useState/final/1-error-example';
+import Final from './tutorial/1-useState/final/3-useState-array';
 
 function App() {
   return (
     <div className='container'>
       <Setup/>
+      <Final/>
     </div>
   )
 }
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -9,6 +9,15 @@ const UseStateCounter = () => {
   const resetNumber = () => {
     return setValue(0)
   }
+
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      // setValue(value + 1)
+      setValue((prevState)=> {
+        return prevState + 1;
+      })
+    }, 2000)
+  }
   return (
     <>
       <section style={{margin:'4rem 0'}}>
@@ -17,6 +26,11 @@ const UseStateCounter = () => {
         <button className="btn" onClick={decreaseNumber}>decrease</button>
         <button className="btn" onClick={resetNumber}>reset</button>
         <button className="btn" onClick={()=> {setValue(value + 1)}}>increase</button>
+      </section>
+      <section>    
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease} >increase later</button>
       </section>
     </>
   );
